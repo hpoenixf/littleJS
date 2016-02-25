85daaece8fd917c0d36c620b07f0e365c8026fff
hpoenixf <hpfengx@gamil.com>
2016-02-25 14:12:00 +0800

    'off'

--- Qdelegate.js
+++ Qdelegate.js
@@ -1,40 +1,55 @@
-
 //inspired by oui-dom-events https://github.com/oneuijs/oui-dom-events/blob/master/src/index.js
 
 
 function $q(selector) {
     var el = document.querySelectorAll(selector)
     return new onObj(el)
-}
 
-function onObj(el) {
-    this.el = el
-}
+    function onObj(el) {
+        this.el = el
+        this.fn = []
+        this.fnName = []
+        onObj.prototype.on = function(eve, selector, fn) {
+            var el = this.el;
+            if (selector instanceof Function) {
+                fn = selector;
+                els = el
+            } else {
+                els = document.querySelectorAll(selector)
+            }
+            var matched;
+            // 判断有无选择器            
+            this.fn.push(
+                function(e) {
+                    for (var i = 0; i < els.length; i++) {
+                        var _el = els[i]
+                        if (_el === e.target || _el.contains(e.target)) {
+                            matched = _el;
+                            break;
+                        }
+                    }
+                    if (matched) {
+                        fn.apply(matched, [].slice.call(arguments));
+                    }
+                })
+            this.fnName.push(fn.name)
 
-onObj.prototype.on = function(eve, selector, fn) {
-    var el = this.el;
-    // 判断有无选择器
-    if (selector instanceof Function) {
-        fn = selector;
-        els = el
-    } else {
-        els = document.querySelectorAll(selector)
-    }
-    var matched;
-    for (var j = 0; j < el.length; j++) {
-        el[j].addEventListener(eve, function(e) {
-            for (var i = 0; i < els.length; i++) {
-                var _el = els[i]
-                    // contains,node节点是否另一另一节点的父元素，来自司徒正美的js框架设计
-                if (_el === e.target || _el.contains(e.target)) {
-                    matched = _el;
-                    break;
+            for (var j = 0; j < el.length; j++) {
+                el[j].addEventListener(eve, this.fn[this.fn.length - 1])
+            }
+        }
+        onObj.prototype.off = function(eve, fn) {
+            fn = !(fn instanceof Function) && arguments[2] ? arguments[2] : fn
+
+            var el = this.el;
+            var index = this.fnName.indexOf(fn.name)
+            if (index !== -1) {
+                for (var j = 0; j < el.length; j++) {
+                    el[j].removeEventListener(eve, this.fn[index])
                 }
+                this.fn.splice(index, 1)
             }
-            if (matched) {
-                fn.apply(matched, [].slice.call(arguments));
-            }
-        })
+        }
     }
 }
 
@@ -42,6 +57,12 @@ onObj.prototype.on = function(eve, selector, fn) {
 
 
 //example
-//$q('#ntp-contents').on('click', 'div', function() {
-//    console.log(this)
-//})
+//不接触的话，可以使用匿名函数，可以链式绑定
+//$q('#ntp-contents').on('click', 'div', fn)
+//如果要解除事件代理，不能绑定匿名函数，不能链式绑定，之后再接触
+// var  a  = $q('#ntp-contents')
+// a.on('click', 'div', fn)
+//a.off('click', 'div', fn)
+//
+// 不支持这样
+// $q('#ntp-contents').off('click', fn)
