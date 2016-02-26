69951cdb2d1f94c1f0c7f9803f250b3b08e5d068
hpoenif <hpfengx@gmail.com>
2016-02-26 20:16:22 +0800

    Refactor

--- Qdelegate.js
+++ Qdelegate.js
@@ -1,14 +1,12 @@
 //inspired by oui-dom-events https://github.com/oneuijs/oui-dom-events/blob/master/src/index.js
 function $q(selector) {
-    // 使用全局变量来保存获取的对象。
-    if (window.$qObj === undefined) {
-        window.$qObj = {}
-    }
+    // 使用全局变量来保存获取的对象。若不存在则新建变量
+    window.$qObj=window.$qObj||{}
     //将选择器替换为可用的变量名，按需要再扩展，已经匹配 '#' '.' ' ' '+'  ':''
     var sel2pro = selector.replace(/\#/g, "$ID").replace(/\./g, "$CL").replace(/\s/g, "$SS").replace(/\+/g, "$AD").replace(/\:/g, "$CO")
+   
     if (window.$qObj[sel2pro] === undefined) {
-        var el = document.querySelectorAll(selector)
-        window.$qObj[sel2pro] = new onObj(el)
+        window.$qObj[sel2pro] = new onObj(document.querySelectorAll(selector))
     }
     return window.$qObj[sel2pro]
 
@@ -16,77 +14,19 @@ function $q(selector) {
         this.el = el
         this.fn = []
         this.fnName = []
-        onObj.prototype.on = function(eve, selector, fn) {
-            var el = this.el;
-            // 判断有无选择器
-            if (selector instanceof Function) {
-                fn = selector;
-                els = el
-            } else {
-                els = document.querySelectorAll(selector)
-            }
-            var matched;
-            //保存函数的名字
-            this.fnName.push(eve+'&'+fn.name)
-                //保存函数
-            this.fn.push(
-                    function(e) {
-                        for (var i = 0; i < els.length; i++) {
-                            var _el = els[i]
-                                //使用contains判断是否两者父子节点的关系，不用冒泡
-                            if (_el === e.target || _el.contains(e.target)) {
-                                matched = _el;
-                                break;
-                            }
-                        }
-                        if (matched) {
-                            fn.apply(matched, [].slice.call(arguments));
-                        }
-                    })
-                //监听事件
-            for (var j = 0; j < el.length; j++) {
-                el[j].addEventListener(eve, this.fn[this.fn.length - 1])
-            }
+        var self = this
+        onObj.prototype.on = function() {
+            bindEvent.apply(this, arguments)
         }
-
-        onObj.prototype.once = function(eve, selector, fn) {
-            var el = this.el;
-            if (selector instanceof Function) {
-                fn = selector;
-                els = el
-            } else {
-                els = document.querySelectorAll(selector)
-            }
-            var matched;
-            this.fnName.push(eve+'&'+fn.name)
-            var that = this
-            this.fn.push(function(e) {
-                for (var i = 0; i < els.length; i++) {
-                    var _el = els[i]
-                    if (_el === e.target || _el.contains(e.target)) {
-                        matched = _el;
-                        break;
-                    }
-                }
-                if (matched) {
-                    fn.apply(matched, [].slice.call(arguments));
-                    //黑科技callee
-                    for (var j = 0; j < el.length; j++) {
-                        el[j].removeEventListener(eve, arguments.callee)
-                    }
-                    var index = that.fnName.indexOf(fn.name)
-                    that.fn.splice(index, 1)
-                    that.fnName.splice(index, 1)
-                }
-            })
-            for (var j = 0; j < el.length; j++) {
-                el[j].addEventListener(eve, this.fn[this.fn.length - 1])
-            }
+        onObj.prototype.once = function() {
+            var arg = [].slice.call(arguments)
+            arg.push(true)
+            bindEvent.apply(self, arg)
         }
         onObj.prototype.off = function(eve, fn) {
             fn = !(fn instanceof Function) && arguments[2] ? arguments[2] : fn
             var el = this.el;
-            var index = this.fnName.indexOf(eve+'&'+fn.name)
+            var index = this.fnName.indexOf(eve + '&' + fn.name)
             if (index !== -1) {
                 for (var j = 0; j < el.length; j++) {
                     el[j].removeEventListener(eve, this.fn[index])
@@ -95,6 +35,46 @@ function $q(selector) {
                 this.fnName.splice(index, 1)
             }
         }
+    }
+
+    function bindEvent(eve, selector, fn, once) {
+
+        var el = this.el;
+        var els;
+        if (selector instanceof Function) {
+            fn = selector;
+            els = el
+        } else {
+            els = document.querySelectorAll(selector)
+        }
+        var matched;
+        this.fnName.push(eve + '&' + fn.name)
+        var that = this
+        this.fn.push(function(e) {
+            for (var i = 0; i < els.length; i++) {
+                var _el = els[i]
+                if (_el === e.target || _el.contains(e.target)) {
+                    matched = _el;
+                    break;
+                }
+            }
+            if (matched) {
+                fn.apply(matched, [].slice.call(arguments));
+                //黑科技callee
+                if (once) {
+                    for (var j = 0; j < el.length; j++) {
+                        el[j].removeEventListener(eve, arguments.callee)
+                    }
+                    var index = that.fnName.indexOf(fn.name)
+                    that.fn.splice(index, 1)
+                    that.fnName.splice(index, 1)
+                }
+            }
+        })
+        for (var j = 0; j < el.length; j++) {
+            el[j].addEventListener(eve, this.fn[this.fn.length - 1])
+        }
+
     }
 }
 
