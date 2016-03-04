9d543ce74a34762ec55b25364f6121099e290806
hpoenif <hpfengx@gmail.com>
2016-03-04 21:05:35 +0800

    'refractor'

--- $q/Qdelegate.js
+++ $q/Qdelegate.js
@@ -12,110 +12,10 @@ function $q(selector) {
     return window.$qObj[sel2pro]
 }
 
-function onObj(el) {
-    var addEvent;
-    var removeEvent;
-    // 条件预加载，不用每次执行都进行重复判定————来自《高性能js》p156
-    // 现代浏览器
-    if (window.addEventListener) {
-        addEvent = function(ele, eve, fn) {
-            ele.addEventListener(eve, fn)
-            return this
-        }
-        removeEvent = function(ele, eve, fn) {
-            ele.removeEventListener(eve, fn);
-            return this
-        }
-        fireEvent = function(target, eve) {
-            var evt = document.createEvent("HTMLEvents");
-            evt.initEvent(eve, true, true);
-            for (var i = 0; i < target.length; i++) {
-                target[i].dispatchEvent(evt);
-            }
-        }
-    } else {
-        // 万恶的ie9-
-        addEvent = function(ele, eve, fn) {
-            ele.attachEvent("on" + eve, fn);
-            return this
-        }
-        removeEvent = function(ele, eve, fn) {
-            ele.detachEvent("on" + eve, fn);
-            return this
-        }
-        fireEvent = function(target, eve) {
-            var evt = document.createEventObject();
-            for (var i = 0; i < target.length; i++) {
-                target[i].fireEvent('on' + event, evt)
-            }
-        }
-    }
-    this.el = el
-    this.fn = []
-    this.fnName = []
-    var self = this
-    onObj.prototype.on = function() {
-        bindEvent.apply(this, arguments)
-        return this
-    }
-    onObj.prototype.once = function() {
-        var arg = [].slice.call(arguments)
-        arg.push(true)
-        bindEvent.apply(self, arg)
-        return this
-    }
-    onObj.prototype.off = function(eve, fn) {
-            fn = !(fn instanceof Function) && arguments[2] ? arguments[2] : fn
-            var el = this.el;
-            var index = this.fnName.indexOf(eve + '&' + fn.name)
-            if (index !== -1) {
-                for (var j = 0; j < el.length; j++) {
-                    removeEvent(el[j], eve, this.fn[index])
-                }
-                this.fn.splice(index, 1)
-                this.fnName.splice(index, 1)
-            }
-            return this
-        }
-        // 取消绑定到该元素的事件eve的所有监听事件
-    onObj.prototype.offEvent = function(eve) {
-        var el = this.el;
-        var index = 0;
-        while (index < this.fnName.length) {
-            if (this.fnName[index].split("&")[0] === eve) {
-                for (var j = 0; j < el.length; j++) {
-                    removeEvent(el[j], eve, this.fn[index])
-                }
-                this.fn.splice(index, 1)
-                this.fnName.splice(index, 1)
-                index--
-            }
-            index++
-        }
-        return this
-    }
-    onObj.prototype.fire = function(eve, selector) {
-        var el = self.el
-        var target = [];
-        if (selector) {
-            var sel = document.querySelectorAll(selector);
-            // 同样是使用contains来判断，不使用冒泡
-            for (var k = 0; k < el.length; k++) {
-                for (var j = 0; j < sel.length; j++) {
-                    if (el[k].contains(sel[j])) {
-                        target.push(sel[j])
-                    }
-                }
-            }
-        } else {
-            target = el
-        }
-
-        fireEvent(target, eve)
-        return this
-    }
-
-    function bindEvent(eve, selector, fn, once) {
+(function() {
+    window.$qEv = {}
+    var $qEv = window.$qEv;
+    $qEv.bindEvent = function(eve, selector, fn, once) {
         var el = this.el;
         var els;
         if (selector instanceof Function) {
@@ -142,7 +42,7 @@ function onObj(el) {
                 //黑科技callee
                 if (once) {
                     for (var j = 0; j < el.length; j++) {
-                        removeEvent(el[j], eve, arguments.callee)
+                        $qEv.removeEvent(el[j], eve, arguments.callee)
                     }
                     var index = that.fnName.indexOf(fn.name)
                     that.fn.splice(index, 1)
@@ -151,10 +51,120 @@ function onObj(el) {
             }
         })
         for (var j = 0; j < el.length; j++) {
-            addEvent(el[j], eve, this.fn[this.fn.length - 1])
+            $qEv.addEvent(el[j], eve, this.fn[this.fn.length - 1])
         }
     }
+    if (window.addEventListener) {
+        $qEv.addEvent = function(ele, eve, fn) {
+            ele.addEventListener(eve, fn)
+            return this
+        }
+        $qEv.removeEvent = function(ele, eve, fn) {
+            ele.removeEventListener(eve, fn);
+            return this
+        }
+        $qEv.fireEvent = function(target, eve) {
+            var evt = document.createEvent("HTMLEvents");
+            evt.initEvent(eve, true, true);
+            for (var i = 0; i < target.length; i++) {
+                target[i].dispatchEvent(evt);
+            }
+        }
+    } else {
+        // 万恶的ie9-
+        $qEv.addEvent = function(ele, eve, fn) {
+            ele.attachEvent("on" + eve, fn);
+            return this
+        }
+        $qEv.removeEvent = function(ele, eve, fn) {
+            ele.detachEvent("on" + eve, fn);
+            return this
+        }
+        $qEv.fireEvent = function(target, eve) {
+            var evt = document.createEventObject();
+            for (var i = 0; i < target.length; i++) {
+                target[i].fireEvent('on' + event, evt)
+            }
+        }
+    }
+})()
+
+
+function onObj(el) {
+    this.el = el
+    this.fn = []
+    this.fnName = []
 }
+
+onObj.prototype = {
+    constructor: onObj,
+
+    on: function() {
+        window.$qEv.bindEvent.apply(this, arguments)
+        return this
+    },
+    once: function() {
+        var arg = [].slice.call(arguments)
+        arg.push(true)
+        window.$qEv.bindEvent.apply(this, arg)
+        return this
+    },
+    off: function(eve, fn) {
+        fn = !(fn instanceof Function) && arguments[2] ? arguments[2] : fn
+        var el = this.el;
+        var index = this.fnName.indexOf(eve + '&' + fn.name)
+        if (index !== -1) {
+            for (var j = 0; j < el.length; j++) {
+                window.$qEv.removeEvent(el[j], eve, this.fn[index])
+            }
+            this.fn.splice(index, 1)
+            this.fnName.splice(index, 1)
+        }
+        return this
+    },
+    // 取消绑定到该元素的事件eve的所有监听事件
+    offEvent: function(eve) {
+        var el = this.el;
+        var index = 0;
+        while (index < this.fnName.length) {
+            if (this.fnName[index].split("&")[0] === eve) {
+                for (var j = 0; j < el.length; j++) {
+                    window.$qEv.removeEvent(el[j], eve, this.fn[index])
+                }
+                this.fn.splice(index, 1)
+                this.fnName.splice(index, 1)
+                index--
+            }
+            index++
+        }
+        return this
+    },
+    fire: function(eve, selector) {
+        var el = this.el
+        var target = [];
+        if (selector) {
+            var sel = document.querySelectorAll(selector);
+            // 同样是使用contains来判断，不使用冒泡
+            for (var k = 0; k < el.length; k++) {
+                for (var j = 0; j < sel.length; j++) {
+                    if (el[k].contains(sel[j])) {
+                        target.push(sel[j])
+                    }
+                }
+            }
+        } else {
+            target = el
+        }
+        window.$qEv.fireEvent(target, eve)
+        return this
+    }
+}
+
+
+
+
+
+
 
 
 //可以使用匿名函数，可以链式绑定
@@ -165,16 +175,17 @@ function onObj(el) {
 // $q('body').el里以数组的形式存储着获取的元素
 // 也可以使用a = new onObj(el)来,a.on(eve,selector,fn)的形式来使用
 //example
-// function con() {
-//     console.log(this)
-// }
-// function bon() {
-//     console.log(new Date)
-// }
+function con() {
+    console.log(this)
+}
 
-// $q('body').on('click', 'div', con)
-// console.log(window.$qObj)
-// $q('body').once('click', 'div', bon)
-// $q('body').off('click', 'div', bon)
-// $q('body').fire('click', 'div')
-// $q('div').off('click')
+function bon() {
+    console.log(new Date)
+}
+
+$q('body').on('click', 'div', con)
+    // console.log(window.$qObj)
+    // $q('body').once('click', 'div', bon)
+    // $q('body').off('click', 'div', bon)
+    // $q('body').fire('click', 'div')
+    // $q('div').off('click')
