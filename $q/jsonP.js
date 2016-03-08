115ad0c4a2b18db3c9147bb3f3ceeb385dbbf4a9
hpoenixf <hpfengx@gmail.com>
2016-03-08 23:28:41 +0800

    fix bug

--- $q/jsonP.js
+++ $q/jsonP.js
@@ -3,7 +3,7 @@ function JsonP(url,data,callback) {
 	jsp.type = "text/javascript"
 	var dataStr = ""
 	for(var i in data) {
-		dataStr+=("&"+escape(i)+"="+escape(obj[i]))
+		dataStr+=("&"+encodeURIComponent(i)+"="+encodeURIComponent(obj[i]))
 	}
 	jsp.src = url+"?callback="+callback+dataStr
 	document.body.appendChild(jsp)
