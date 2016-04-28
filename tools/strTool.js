01cc2957e480222c3926257a45070f357b2a937b
pengfei <hpfengx@gmail.com>
2016-04-28 20:10:37 +0800

    Update strTool.js

--- tools/strTool.js
+++ tools/strTool.js
@@ -1,1 +1,42 @@
-strTool.js
+// from vue source code
+
+
+var camelizeRE = /-(\w)/g
+export function camelize (str) {
+  return str.replace(camelizeRE, toUpper)
+}
+
+function toUpper (_, c) {
+  return c ? c.toUpperCase() : ''
+}
+
+/**
+ * Hyphenate a camelCase string.
+ *
+ * @param {String} str
+ * @return {String}
+ */
+
+var hyphenateRE = /([a-z\d])([A-Z])/g
+export function hyphenate (str) {
+  return str
+    .replace(hyphenateRE, '$1-$2')
+    .toLowerCase()
+}
+
+/**
+ * Converts hyphen/underscore/slash delimitered names into
+ * camelized classNames.
+ *
+ * e.g. my-component => MyComponent
+ *      some_else    => SomeElse
+ *      some/comp    => SomeComp
+ *
+ * @param {String} str
+ * @return {String}
+ */
+
+var classifyRE = /(?:^|[-_\/])(\w)/g
+export function classify (str) {
+  return str.replace(classifyRE, toUpper)
+}
